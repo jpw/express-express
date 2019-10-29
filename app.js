538b944b78dc594fee3256b0ad945bd748d0c46a
jpw <jon@mdi.eu.com>
2019-10-29 16:14:56 +0000

    test ad

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const routeContentMap = {
 		<script type="text/javascript">
 		function OptanonWrapper() { }
 		</script>
-		<!-- OneTrust Cookies Consent Notice end -->`,
+		<!-- OneTrust Cookies Consent Notice end -->
+
+		<div id="div-gpt-ad-right-2" class="div-gpt-ad medium-rectangle advert js-ad text-center hide-print grade-c-hide" data-gpt-unitpath="/285/nature.com/article" data-gpt-sizes="300x250" data-gpt-targeting="pos=right;artid=/articles/d41586-019-03202-2;path=/articles/d41586-019-03202-2" data-ad-type="right" data-google-query-id="CICZkKHuweUCFWxZFQgdwdgEzw">
+    <div id="google_ads_iframe_/270604982/nature/nature/articles_1__container__" style="border: 0pt none;"><iframe id="google_ads_iframe_/270604982/nature/nature/articles_1" title="3rd party ad content" name="google_ads_iframe_/270604982/nature/nature/articles_1" width="300" height="250" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" srcdoc="" data-google-container-id="1" style="border: 0px; vertical-align: bottom;" data-load-complete="true"></iframe></div></div>
+`,
 	},
 	'/alt': {
 		title: 'alt test express',
