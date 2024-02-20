68569a1e4b8a2dcdba4d3166cb16aab93207e4ea
jpw <jpw@users.noreply.github.com>
2024-02-20 15:27:15 +0000

    adds viewport meta tag for iOS sanity

--- template.js
+++ template.js
@@ -3,6 +3,7 @@ module.exports = ({title, section01}) => `<!doctype html>
 <head>
 	<meta charset="utf-8">
 	<title>${title}</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=5,user-scalable=yes">
 	<link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.1/normalize.css">
 	<style>
 		* {
